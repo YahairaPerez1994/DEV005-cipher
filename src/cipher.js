89abe94ec6756b1d890955f4d10c4f46bfdd8f3d
Yahaira Perez <yahairaperez2694@gmail.com>
2023-02-28 08:45:59 -0500

    Este es el primer commit de este archivo

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,40 @@
 const cipher = {
-  // ...
-};
+  encode:function(desplazamiento,mensaje){
+    if(typeof desplazamiento!=="number"||typeof mensaje!=="string"){throw new TypeError("argumentos invalidos");
+    }
+    let mensajecifrado=""
+    for (let A=0;A<mensaje.length;A++){
+      const codigoLetra=mensaje.charCodeAt(A);
+      const codigoLetraMasDesplazamiento = codigoLetra+desplazamiento;
+      const mayusculal=codigoLetra>=65&&codigoLetra<=90;
+      if(mayusculal){
+        const codigoLetraForm=((codigoLetraMasDesplazamiento-65)%26)+65;
+        const codigoLetraResul=String.fromCharCode(codigoLetraForm);
+        mensajecifrado+=codigoLetraResul;
+      }
+      else{mensajecifrado+=mensaje[A];
+      } 
+    }
+    return mensajecifrado;
 
+  },
+  decode:function(desplazamiento,mensaje){
+    if(typeof desplazamiento!=="number"||typeof mensaje!=="string"){throw new TypeError("argumentos invalidos");
+    }
+    let mensajedescifrado=""
+    for (let A=0;A<mensaje.length;A++){
+      const codigoLetra=mensaje.charCodeAt(A);
+      const codigoLetraMasDesplazamiento = codigoLetra-desplazamiento;
+      const mayusculal=codigoLetra>=65&&codigoLetra<=90;
+      if(mayusculal){
+        const codigoLetraForm=((codigoLetraMasDesplazamiento+65)%26)+65;
+        const codigoLetraResul=String.fromCharCode(codigoLetraForm);
+        mensajedescifrado+=codigoLetraResul
+      }
+      else{mensajedescifrado+=mensaje[A]
+      }          
+    }
+    return mensajedescifrado
+  }
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const botoncifrado = document.getElementById("cifrar");
+const botondescifrado = document.getElementById("descifrar");
+
+botoncifrado.addEventListener ("click", function(){
+  const mensaje = document.getElementById ("mensaje1").value;
+  const desplazamiento = document.getElementById ("desplazamiento").value;
+  const mensaje2 = cipher.encode(parseInt(desplazamiento),mensaje);
+  document.getElementById("mensaje2").value= mensaje2;
+});
+
+botondescifrado.addEventListener ("click", function(){
+  const mensaje = document.getElementById("mensaje1").value;
+  const desplazamiento = document.getElementById("desplazamiento").value;
+  const mensaje2 = cipher.decode(parseInt(desplazamiento),mensaje);
+  document.getElementById("mensaje2").value= mensaje2;
+});
+
+//console.log(cipher);
+//Declaro variables que utilizare en el DOM
+//const cifrar = document.getElementById("cifrar").value;//RESULTADO
+//const descifrar = document.getElementById("descifrar").value;//RESULTADO
+
+//cifrar.addEventListener ("click", function(){
+//  const desplazamiento = document.getElementById("desplazamiento").value;
+//  const mensaje1 = document.getElementById("mensaje1").value;
+//  const mensaje2 = cipher.encode(+desplazamiento,mensaje1);
+//  document.getElementById("mensaje2").value= mensaje2;
+//});
+//descifrar.addEventListener ("click", function(){
+//  const desplazamiento = document.getElementById("desplazamiento").value;
+//  const mensaje1 = document.getElementById("mensaje1").value;
+//  const mensaje2 = cipher.decode(+desplazamiento,mensaje1);
+//  document.getElementById("mensaje2").value= mensaje2;
+//})
+
